bc1928a06397274bc9c1b71d6707fd1dd0094881
Paradispartiet <paradispartiet@gmail.com>
2026-08-16 23:10:12 +0200

    feat(api): register dish search module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from "@nestjs/common";
+import { DishSearchController } from "./dish-search.controller.js";
+import { DishSearchService } from "./dish-search.service.js";
 import { HealthController } from "./health.controller.js";
 
 @Module({
-  controllers: [HealthController],
+  controllers: [HealthController, DishSearchController],
+  providers: [DishSearchService],
 })
 export class AppModule {}
