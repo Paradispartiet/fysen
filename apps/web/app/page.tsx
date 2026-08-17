cd90d2979c1bab178be988160cd13412bb9e15af
Paradispartiet <paradispartiet@gmail.com>
2026-08-17 09:43:12 +0200

    Integrer matskisser i Fysen-forsiden

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,5 @@
 import { DishSearch } from "../components/dish-search";
+import { FoodSketches } from "../components/food-sketches";
 import { GlobalHeader } from "../components/global-header";
 
 export default function HomePage() {
@@ -7,13 +8,16 @@ export default function HomePage() {
       <GlobalHeader />
       <main className="homeMain">
         <section className="homeHero" aria-labelledby="fysen-title">
-          <h1 id="fysen-title">
-            <span>Hva har du</span>
-            <span>lyst på?</span>
-          </h1>
-          <div className="homeSearchWrap">
-            <DishSearch />
-            <p className="searchProof">Søk i ferske, sporbare restaurantmenyer.</p>
+          <FoodSketches />
+          <div className="homeHeroContent">
+            <h1 id="fysen-title">
+              <span>Hva har du</span>
+              <span>lyst på?</span>
+            </h1>
+            <div className="homeSearchWrap">
+              <DishSearch />
+              <p className="searchProof">Søk i ferske, sporbare restaurantmenyer.</p>
+            </div>
           </div>
         </section>
       </main>
