7fabf55f4913abc0180fbd8d2179d058792fcd52
Paradispartiet <paradispartiet@gmail.com>
2026-08-17 07:50:02 +0200

    feat(api): register revenue funnel services

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
+import { DatabaseService } from "./database.service.js";
 import { DishSearchController } from "./dish-search.controller.js";
 import { DishSearchService } from "./dish-search.service.js";
+import { FunnelController } from "./funnel.controller.js";
+import { FunnelService } from "./funnel.service.js";
 import { HealthController } from "./health.controller.js";
 
 @Module({
-  controllers: [HealthController, DishSearchController],
-  providers: [DishSearchService],
+  controllers: [HealthController, DishSearchController, FunnelController],
+  providers: [DatabaseService, DishSearchService, FunnelService],
 })
 export class AppModule {}
