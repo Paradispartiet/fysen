c46c793cef6d24575421c03a51c742896af3f7dd
Paradispartiet <paradispartiet@gmail.com>
2026-08-17 08:03:59 +0200

    feat(database): export restaurant action persistence

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -3,4 +3,5 @@ export * from "./due-sources.js";
 export * from "./funnel.js";
 export * from "./migrate.js";
 export * from "./repository.js";
+export * from "./restaurant-actions.js";
 export * from "./search.js";
