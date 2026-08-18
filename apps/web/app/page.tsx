4854a5f66b4ba7ddb4940ada66bc60855de8e422
Paradispartiet <paradispartiet@gmail.com>
2026-08-18 12:56:57 +0200

    Gjør søkeforklaringen mer direkte

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -21,7 +21,7 @@ export default function HomePage() {
           </div>
           <div className="homeSearchWrap">
             <DishSearch />
-            <p className="searchProof">Søk etter en rett og se hvilke restauranter som har den på en fersk, sporbar meny.</p>
+            <p className="searchProof">Søk etter en rett og se hvilke restauranter som har den på menyen nå.</p>
           </div>
         </section>
         <CuisineExplorer />
