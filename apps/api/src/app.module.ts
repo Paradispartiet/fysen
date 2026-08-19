76ad651201e5dbd60f0f94fded88f74a86630973
Paradispartiet <paradispartiet@gmail.com>
2026-08-19 16:33:21 +0200

    Register Fysen Pro API

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,12 +4,20 @@ import { DishSearchController } from "./dish-search.controller.js";
 import { DishSearchService } from "./dish-search.service.js";
 import { FunnelController } from "./funnel.controller.js";
 import { FunnelService } from "./funnel.service.js";
+import { FysenProController } from "./fysen-pro.controller.js";
+import { FysenProService } from "./fysen-pro.service.js";
 import { HealthController } from "./health.controller.js";
 import { RestaurantClaimsController } from "./restaurant-claims.controller.js";
 import { RestaurantClaimsService } from "./restaurant-claims.service.js";
 
 @Module({
-  controllers: [HealthController, DishSearchController, FunnelController, RestaurantClaimsController],
-  providers: [DatabaseService, DishSearchService, FunnelService, RestaurantClaimsService],
+  controllers: [
+    HealthController,
+    DishSearchController,
+    FunnelController,
+    RestaurantClaimsController,
+    FysenProController,
+  ],
+  providers: [DatabaseService, DishSearchService, FunnelService, RestaurantClaimsService, FysenProService],
 })
 export class AppModule {}
