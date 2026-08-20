b8d0af7b9a1f3a32f8abea999bcb189c5c898c4f
Paradispartiet <paradispartiet@gmail.com>
2026-08-20 16:01:18 +0200

    Koble Matlyst v2-stiler til webappen

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -10,6 +10,7 @@ import "../styles/design-v2-2.css";
 import "../styles/design-v2-2-1.css";
 import "../styles/dish-learning-dialog.css";
 import "../styles/cuisine-explorer-v2-3.css";
+import "../styles/matlyst-v2.css";
 import "../styles/food-knowledge-v1.css";
 import "../styles/dish-knowledge-restaurants.css";
 import "../styles/discovery-quality-v1.css";
