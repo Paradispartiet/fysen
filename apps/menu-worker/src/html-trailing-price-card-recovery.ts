ea1476521c66c9bbfb2edd8fa22c6a8de868cf38
Paradispartiet <paradispartiet@gmail.com>
2026-09-11 12:21:18 +0200

    Bump trailing-card reconciliation version

--- apps/menu-worker/src/html-trailing-price-card-recovery.ts
+++ apps/menu-worker/src/html-trailing-price-card-recovery.ts
@@ -9,7 +9,7 @@ import { recoverSemanticCategoryCardHtmlItems } from "./html-category-card-recov
 import { looksLikeHtmlDescription } from "./html-description-title-recovery.js";
 
 export const HTML_TRAILING_PRICE_CARD_RECOVERY_VERSION =
-  "trailing-price-card-v18";
+  "trailing-price-card-v19";
 
 const HEADING_MARKER = "__FYSEN_TRAILING_PRICE_HEADING_LEVEL_";
 const PURE_PRICE_LINE =
